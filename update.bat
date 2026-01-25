1f67ca8aa5802595b998e7da663b7fabafe2d464
BAD-JamesWW <anono4021@protonmail.com>
2026-01-24 23:28:15 -0500

    Addressing Bug ID: Bug012420261121PM
    Some Windows setups may not recognize the 'py' command (Python Launcher), so I use 'python' instead

--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Installing/updating required packages...
-py -m pip install --upgrade pip
-py -m pip install -r requirements.txt
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 echo.
 echo All dependencies are installed.
 pause
